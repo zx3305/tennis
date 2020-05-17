6791a6262bc2866702f1cbbbc731799674b29c1a
zero <zero@192.168.0.157>
2020-05-17 15:10:03 +0800

    修改随机梯度下降的算法

--- line/line_stock.py
+++ line/line_stock.py
@@ -32,8 +32,8 @@ def linearReg():
 
 #手动推导梯度下降法
 def gradientDescent():
-	rate = 0.00003	#定义学习率
-	nums = 1100		#梯度下降步数
+	rate = 0.0001	#定义学习率
+	nums = 1000		#梯度下降步数
 	r = 0.000001		#正则化系数
 
 	df = pd.read_csv('./stock_train.csv')
@@ -49,22 +49,20 @@ def gradientDescent():
 
 		#划分训练集与测试集
 	x_train,x_test,y_train,y_test = train_test_split(feature, target, test_size=0.25)
-
-	X = x_train.values
-	Y = y_train.values
 	theta = np.zeros((1, 5))
 	temp = np.zeros(theta.shape)		#初始化矩阵
 	parameters = 5    #计算需要求解的thata参数个数
-	m = len(X)
-
+	m = len(x_train)
 	for i in range(nums):
-		error = np.dot(X,theta.T) - Y 		#对应函数hθ(x) - y
+		#随机梯度下降法
+		index = np.random.randint(0, len(x_train))
+		X = x_train.sample(n=index).values
+		Y = y_train.sample(n=index).values
 
-		if random.randint(0,9) > 3:		#随机梯度下降
-			continue
+		error = np.dot(X,theta.T) - Y 		#对应函数hθ(x) - y
 		for j in range(parameters):
 			#对应公式 θ(j) = θ(j)*(1-λ*α/m) - α/m*∑(hθ(x) - y)*x(j), 这里引入了正则化系数
-			t = np.multiply(error, X[:, j])
+			t = np.multiply(error, X[:, j]) #数惩
 			temp[0, j] = theta[0, j]*(1-r*(rate/m)) - (rate/m)*np.sum(t)
 		theta = temp
 
@@ -75,9 +73,9 @@ def gradientDescent():
 	print("r2_score:",r2_score(y_test, y_hat))
 	print("MAE:", metrics.mean_absolute_error(y_test, y_hat))
 	print("MSE:", metrics.mean_squared_error(y_test, y_hat))
-	plt.scatter(x_test['open'], y_test, c='red',alpha = 0.8)
-	plt.scatter(x_test['open'], y_hat, c='#0909ef', alpha = 0.4)
-	plt.show()
+	# plt.scatter(x_test['open'], y_test, c='red',alpha = 0.8)
+	# plt.scatter(x_test['open'], y_hat, c='#0909ef', alpha = 0.4)
+	# plt.show()
 
 #最小二乘法
 def LeastSquares():
